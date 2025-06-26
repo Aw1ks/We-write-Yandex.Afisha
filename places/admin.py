a9e6291b3cf9aea903774275988c0069e0f7fea9
Aw1ks <aw1ksss331@gmail.com>
2025-06-26 18:20:34 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Pictures_plase
+
+
+class Pictures_plaseInline(admin.TabularInline):
+    model = Pictures_plase
+    extra = 1
+    fields = ('image', 'order',)
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
+    inlines = [Pictures_plaseInline,]
+
+
+@admin.register(Pictures_plase)
+class Pictures_plaseAdmin(admin.ModelAdmin):
+    list_display = ('place', 'order')
+    list_filter = ('place',)
--- places/models.py
+++ places/models.py
@@ -1,13 +1,38 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Place(models.Model):
     title = models.CharField('Название экскурсии', max_length=200)
-    description_short = models.TextField('Краткое описание экскурсии', default='Описание в разработке', blank=True)
-    description_long = models.TextField('Подробное описание экскурсии', default='Описание в разработке', blank=True)
+    description_short = models.TextField('Краткое описание экскурсии', 
+                                        default='Описание в разработке', 
+                                        blank=True)
+    description_long = models.TextField('Подробное описание экскурсии', 
+                                        default='Описание в разработке', 
+                                        blank=True)
 
     latitude = models.FloatField('Широта')
     longitude = models.FloatField('Долгота')
 
     def __str__(self):
         return self.title
+
+
+class Pictures_plase(models.Model):
+    place = models.ForeignKey(Place, 
+                            on_delete=models.CASCADE, 
+                            related_name='pictures')
+
+    image = models.ImageField('Изображение места', 
+                            upload_to='static/images', 
+                            blank=True)
+
+    order = models.PositiveIntegerField('Номер изображения', 
+        default=1,
+        validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return self.place.title
+
+    class Meta:
+        ordering = ['order']
