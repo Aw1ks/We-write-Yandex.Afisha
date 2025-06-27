811f82475086d4afe5fa75b65356776ff64d3125
Aw1ks <aw1ksss331@gmail.com>
2025-06-27 19:12:57 +0300

    frontend takes data from views.py

--- places/views.py
+++ places/views.py
@@ -1,37 +1,29 @@
-from django.shortcuts import render
 import json
+
+from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
     places = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "../static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "../static/places/roofs24.json"
-                }
-            }
-        ]
+        'type': 'FeatureCollection',
+        'features': []
     }
+
+    for place in Place.objects.all():
+        place_layout = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.latitude, place.longitude]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': '../static/places/moscow_legends.json' # Тестовые данные
+            }
+        }
+        places['features'].append(place_layout)
 
     context = {
         'places_json': json.dumps(places)
